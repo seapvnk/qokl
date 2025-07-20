fff70195991c2daf35016a01b8167de2a2b9cda4
Pedro Sérgio <www.psro@gmail.com>
2025-07-20 05:18:47 -0300

    feat(entity): add find by id

--- core/entity.go
+++ core/entity.go
@@ -3,6 +3,7 @@ package core
 import (
 	"encoding/json"
 	"log"
+	"strings"
 
 	badger "github.com/dgraph-io/badger/v4"
 	"github.com/glycerine/zygomys/zygo"
@@ -13,15 +14,15 @@ import (
 * # Entity
 *
 * ## entity in storage:
-* entity.entityid // pk
-* components.component.entityid // value of component
-* rel.relname.entityid1.entityid2 // relationship data
-* tag.tagname.entityid // tag flag for entity
+* entities.entityid // pk
+* components.entityid.component // value of component
+* rels.relname.entityid1.entityid2 // relationship data
+* tags.entityid.tagname // tag flag for entity
 *
 * ## entity api:
 * (insert %(admin user) name: "Pedro" age: 23)
 * (tag admin: myEntity) // can be entity id or entity hash (with id key inside)
-* (fetch admin: myEntity) // can be entity id or entity hash (with id key inside), get exact entity by id
+* (entity myEntity) // can be entity id or entity hash (with id key inside), get exact entity by id
 * (relationship myEntity yourEntity are: %friends %(for 10 years)) // are for both sides, belongs <-, has ->
 * (relationOf myEntity yourEntity) // fetch all relationships between these two
 * (relationsOf myEntity %friends are: %(for 10 years) has: %(meet years ago)) // fetch every which meet criteraa
@@ -50,6 +51,7 @@ func OpenDB() {
 // Entity module setup
 func (vm *VM) UseEntityModule() *VM {
 	vm.environment.AddFunction("insert", fnEntityInsert)
+	vm.environment.AddFunction("entity", fnEntityGet)
 
 	return vm
 }
@@ -72,6 +74,10 @@ func makeTagEntry(tagName string, entityID string) []byte {
 
 func makeEntityComponentEntry(componentName string, entityID string) []byte {
 	return []byte("components." + entityID + "." + componentName)
+}
+
+func makeEntityComponentQuery(entityID string) []byte {
+	return []byte("components." + entityID + ".")
 }
 
 // fnEntityInsert insert an entity at database
@@ -147,4 +153,66 @@ func fnEntityInsert(env *zygo.Zlisp, name string, args []zygo.Sexp) (zygo.Sexp, 
 
 	obj["id"] = objID
 	return toSexp(env, obj), nil
+}
+
+// fnEntityGet an entity at database
+// Lisp (entity myEntity)
+func fnEntityGet(env *zygo.Zlisp, name string, args []zygo.Sexp) (zygo.Sexp, error) {
+	if len(args) != 1 {
+		return zygo.SexpNull, zygo.WrongNargs
+	}
+
+	var objID string
+	// extract obj id
+	switch val := args[0].(type) {
+	case *zygo.SexpStr:
+		objID = val.S
+	case *zygo.SexpHash:
+		for _, pairList := range val.Map {
+			for _, pair := range pairList {
+				kstr := pair.Head.(*zygo.SexpSymbol)
+				if kstr.Name() == "id" {
+					value, ok := pair.Tail.(*zygo.SexpStr)
+					if !ok {
+						return zygo.SexpNull, zygo.WrongNargs
+					}
+					objID = value.S
+					break
+				}
+			}
+		}
+	}
+
+	// build entity hash
+	entityHash := zygo.SexpHash{
+		Map: make(map[int][]*zygo.SexpPair),
+	}
+
+	edb.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		query := makeEntityComponentQuery(objID)
+		prefix := []byte(query)
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			key := strings.Replace(string(item.Key()), string(query), "", int(1))
+			item.Value(func(v []byte) error {
+				var itemValue StoredValue
+				err := json.Unmarshal(v, &itemValue)
+				if err != nil {
+					return nil
+				}
+				keySexp := env.MakeSymbol(key)
+				valSexp := toSexp(env, itemValue.Value)
+				entityHash.HashSet(keySexp, valSexp)
+				return nil
+			})
+		}
+
+		return nil
+	})
+
+	entityHash.HashSet(env.MakeSymbol("id"), toSexp(env, objID))
+
+	return &entityHash, nil
 }
