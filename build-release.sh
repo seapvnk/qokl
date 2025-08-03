15f734ae2ff69ea936df416849d76c095a7bbccf
Pedro Sérgio <www.psro@gmail.com>
2025-08-02 23:48:32 -0300

    fix: fix image push

--- build-release.sh
+++ build-release.sh
@@ -8,9 +8,10 @@ GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o qokl .
 
 tar -czvf qokl-linux-amd64-$VERSION.tar.gz qokl
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker build -t seapvnk/qokl:$VERSION .
 docker tag seapvnk/qokl:$VERSION seapvnk/qokl:latest
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push seapvnk/qokl:$VERSION
 docker push seapvnk/qokl:latest
