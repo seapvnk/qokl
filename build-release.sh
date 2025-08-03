0f255f0f9f560296fb85734d9523636c147f1c28
Pedro Sérgio <www.psro@gmail.com>
2025-08-03 00:15:46 -0300

    fix: change docker login

--- build-release.sh
+++ build-release.sh
@@ -8,7 +8,7 @@ GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o qokl .
 
 tar -czvf qokl-linux-amd64-$VERSION.tar.gz qokl
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 docker build -t seapvnk/qokl:$VERSION .
 docker tag seapvnk/qokl:$VERSION seapvnk/qokl:latest
